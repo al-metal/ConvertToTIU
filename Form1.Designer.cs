092d5ed7dfd67bce45b8bcf127f32932864e77b6
al-metal <al-metal@bk.ru>
2016-07-29 13:57:23 +0400

    Rename form
    add btn, add webRequest

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(381, 44);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Начать загрузку";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(620, 311);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Загрузить товары на ТИУ";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using web;
 
 namespace ConvertToTIU
 {
     public partial class Form1 : Form
     {
+        WebRequest webRequest = new WebRequest();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string otv = null;
+            otv = webRequest.PostRequest("https://my.tiu.ru/cabinet/sign-in");
         }
     }
 }
