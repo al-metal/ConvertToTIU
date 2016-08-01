82612730cac1840e26537e2011a2a9d33eadba30
al-metal <al-metal@bk.ru>
2016-08-01 10:38:52 +0400

    autorizacia

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +15,7 @@ namespace ConvertToTIU
 {
     public partial class Form1 : Form
     {
-        WebRequest webRequest = new WebRequest();
+        web.WebRequest webRequest = new web.WebRequest();
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +24,45 @@ namespace ConvertToTIU
         private void btnLoad_Click(object sender, EventArgs e)
         {
             string otv = null;
-            otv = webRequest.PostRequest("https://my.tiu.ru/cabinet/sign-in");
+            CookieContainer cookie = cookieURL("http://izhevsk.tiu.ru/");
+            otv = PostRequest("https://my.tiu.ru/cabinet/sign-in", cookie);
+        }
+
+        public CookieContainer cookieURL(string url)
+        {
+            CookieContainer cookie = new CookieContainer();
+
+            HttpWebResponse res = null;
+            HttpWebRequest req = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            req.CookieContainer = cookie;
+            res = (HttpWebResponse)req.GetResponse();
+            StreamReader ressr = new StreamReader(res.GetResponseStream());
+            String otv = ressr.ReadToEnd();
+            res.GetResponseStream().Close();
+            req.GetResponse().Close();
+
+            return cookie;
+        }
+
+        public string PostRequest(string nethouseTovar, CookieContainer cookie1)
+        {
+            string otv = null;
+            HttpWebResponse res = null;
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(nethouseTovar);
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = cookie1;
+            res = (HttpWebResponse)req.GetResponse();
+            StreamReader ressr = new StreamReader(res.GetResponseStream());
+            otv = ressr.ReadToEnd();
+  
+
+            return otv;
         }
     }
 }
