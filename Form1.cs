80103be8699c50a6ace601af529d65bb82dda63c
al-metal <al-metal@bk.ru>
2016-08-01 13:39:37 +0400

    Authorizacion tiu.ru

--- Form1.cs
+++ Form1.cs
@@ -23,9 +23,7 @@ namespace ConvertToTIU
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
-            string otv = null;
-            CookieContainer cookie = cookieURL("http://izhevsk.tiu.ru/");
-            otv = PostRequest("https://my.tiu.ru/cabinet/sign-in", cookie);
+            Authorizacion();
         }
 
         public CookieContainer cookieURL(string url)
@@ -43,6 +41,8 @@ namespace ConvertToTIU
             res.GetResponseStream().Close();
             req.GetResponse().Close();
 
+            string str = res.Cookies[0].Value;
+
             return cookie;
         }
 
@@ -54,7 +54,7 @@ namespace ConvertToTIU
             HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(nethouseTovar);
             req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
             req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
-            req.Method = "POST";
+            req.Method = "GET";
             req.ContentType = "application/x-www-form-urlencoded";
             req.CookieContainer = cookie1;
             res = (HttpWebResponse)req.GetResponse();
@@ -63,6 +63,53 @@ namespace ConvertToTIU
   
 
             return otv;
+        }
+
+        public void Authorizacion()
+        {
+            CookieContainer cookie = new CookieContainer();
+
+            HttpWebResponse res = null;
+            HttpWebRequest req = (HttpWebRequest)System.Net.WebRequest.Create("http://izhevsk.tiu.ru/");
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            req.CookieContainer = cookie;
+            res = (HttpWebResponse)req.GetResponse();
+            StreamReader ressr = new StreamReader(res.GetResponseStream());
+            String otv = ressr.ReadToEnd();
+            res.GetResponseStream().Close();
+            req.GetResponse().Close();
+            Cookie ck = res.Cookies["csrf_token"];
+
+            Cookie token = new Cookie("csrf_token", res.Cookies[0].Value);
+            string toke = res.Cookies[0].Value;
+
+            req = (HttpWebRequest)HttpWebRequest.Create("https://my.tiu.ru/cabinet/sign-in");
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            req.Method = "GET";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = cookie;
+            res = (HttpWebResponse)req.GetResponse();
+            ressr = new StreamReader(res.GetResponseStream());
+            otv = ressr.ReadToEnd();
+
+            req = (HttpWebRequest)HttpWebRequest.Create("https://my.tiu.ru/cabinet/auth/phone_login");
+            req.Accept = "application/json, text/javascript, */*; q=0.01";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+            req.Method = "POST";
+            req.Referer = "https://my.tiu.ru/cabinet/sign-in";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Headers.Add("X-CSRFToken", toke);
+            req.Headers.Add("Origin", "https://my.tiu.ru");
+            req.CookieContainer = cookie;
+            //req.CookieContainer.Add(cCol);
+            byte[] ms = Encoding.ASCII.GetBytes("phone_email=moto%40bike18.ru&password=TIURU12345&csrf_token=" + toke + "&_save=YES&");
+            req.ContentLength = ms.Length;
+            Stream stre = req.GetRequestStream();
+            stre.Write(ms, 0, ms.Length);
+            stre.Close();
+            res = (HttpWebResponse)req.GetResponse();
         }
     }
 }
